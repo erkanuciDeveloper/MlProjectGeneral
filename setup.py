f948aece3d44c9abf30a20970f20824396861239
Erkan UCI <erkan48592@hotmail.com>
2024-01-28 17:44:04 +0000

    rev of setup.py

--- setup.py
+++ setup.py
@@ -31,5 +31,11 @@ setup(
     author='Erkan',
     author_email='erkan48592@hotmail.com',
     packages=find_packages(),
+    classifiers=[
+        'Programming Language :: Python :: 3',
+        'License :: OSI Approved :: MIT License',
+        'Operating System :: OS Independent',
+    ],
     install_requires=get_requirements(REQUIREMENTS_FILE)
+ 
 )
