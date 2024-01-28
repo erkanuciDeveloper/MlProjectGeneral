df1aec4f6e626587214c94b77c834c962590b6eb
Erkan UCI <erkan48592@hotmail.com>
2024-01-28 17:20:33 +0000

    Setup and reqirements.txt file are done

---
+++ setup.py
@@ -0,0 +1,35 @@
+from setuptools import setup, find_packages
+from typing import List
+
+# Define the file path for requirements.txt
+REQUIREMENTS_FILE = 'requirements.txt'
+
+# Define the string for -e . entry
+HYPHEN_E_DOT = '-e .'
+
+def get_requirements(file_path: str) -> List[str]:
+    '''
+    This function returns the list of requirements from the given file.
+    '''
+    requirements = []
+    try:
+        with open(file_path, 'r') as file:
+            requirements = file.readlines()
+            # Remove newline characters from each requirement
+            requirements = [req.strip() for req in requirements]
+            # Remove the -e . entry if present
+            if HYPHEN_E_DOT in requirements:
+                requirements.remove(HYPHEN_E_DOT)
+    except FileNotFoundError:
+        print(f"Error: {file_path} not found.")
+    return requirements
+
+setup(
+    name='MlProjectGeneral_package',
+    version='1.0.0',
+    description='A sample Python package',
+    author='Erkan',
+    author_email='erkan48592@hotmail.com',
+    packages=find_packages(),
+    install_requires=get_requirements(REQUIREMENTS_FILE)
+)
