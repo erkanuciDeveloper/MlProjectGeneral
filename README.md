29a17791552b808b6f22365da19168523698d3ff
Erkan UCI <erkan48592@hotmail.com>
2024-05-23 13:18:51 +0100

    data_ingestion and data transformartion

---
+++ README.md
@@ -0,0 +1,29 @@
+
+# Create a git repository
+# Add the project to git
+# Create a requirements.txt file to list project dependencies
+# Define project metadata and dependencies in setup.py
+# Create __init__.py in the src folder
+# Set up a virtual environment for the project
+# Activate the virtual environment
+# Install project dependencies from the requirements.txt file 
+# pip install - r requirements.txt -this code will triger the setup file , because we added the -e into requirements.txt.
+# When we run pip install -r requirements.txt, it will trigger the setup file if the -e flag is included in the requirements.txt file. 
+# Each folder where you need to use it as a package or module should have an __init__.py file
+# Create a Components folder and an __init__.py file
+# Create a data_ingestion.py file in the Components folder
+# Create another file under the Components folder
+# Create an exceptions.py file for managing exception handling
+# Create a logger.py file for logging
+# Create a utilities file and add functions that all project components need to use
+# Create a pipeline folder for the model pipeline
+
+
+# Now create data ingestion process into Data ingestion file.
+# Now create data_tranformation 
+# Add some fuction into utils
+
+
+
+
+
---
+++ src/components/data_transformation.py
@@ -0,0 +1,167 @@
+# main purpose of data transformation:
+#1 Feature engineering
+#2 Data cleaning
+#3 Data processing
+#4 Categorical and Nume
+
+
+import sys
+from dataclasses import dataclass
+
+import numpy as np
+import pandas as pd
+from sklearn.compose import ColumnTransformer
+# ColumnTransformer libray helpp to pipeline to encoding data
+from sklearn.impute import SimpleImputer # for missing value we can use this
+from sklearn.pipeline import Pipeline
+from sklearn.preprocessing import OneHotEncoder,StandardScaler
+
+from src.exception import CustomException
+from src.logger import logging
+import os
+import sys
+
+@dataclass
+class DataTransformationConfig:
+    # for save model we use this path
+    preprocessor_obj_file_path=os.path.join('artifacts','preprocessor.pkl')
+
+
+class DataTransformation:
+    def __init__(self):
+        self.data_transformation_config=DataTransformationConfig()
+
+
+    def get_data_transformer_object(self):
+        '''
+        This function is responsible for data transformation for train data
+        '''
+
+        try:
+            numeric_columns = [
+                'writing_scrore',
+                'readning_scroe'
+                ]
+            categorical_columns=[
+                'gender',
+                'race_ethnicity',
+                'parental_level_of_education',
+                'lunch',
+                'test_preparation_course'              
+                                
+                ]
+            
+            #create piple handling mising values and doing the standart sclaer
+            # This pipeline for traing data set, how we do fittranform in train data set, this for this
+            # Transfor just for test data set, fit_transform for traing data set
+            num_pipeline=Pipeline(
+                steps=[
+                      #handilin missing falues
+                    ('imputer',SimpleImputer(strategy='median')) #handling missing value
+                    ('scaler',StandardScaler())
+
+                ]
+
+            )
+
+            #for missing value
+            #for hanglin categorical features converting into numerical values
+            # then last step do scaler
+            cat_pipeline=Pipeline(
+                steps=[
+                    ('imputer', SimpleImputer(strategy='most_frequent'))# for missing value
+                    ('one_hot_encoder',OneHotEncoder())# apply one hot encodeer to convert categorical value to numerical values
+                    ('scaler',StandardScaler())
+                ]              
+
+            )
+
+            logging.info('Numerical columns standart scaler completed')
+            logging.info('Categorical columns encoding completed')
+
+
+
+            #Now we can give the this pipeline above processs
+            preprocess=ColumnTransformer(
+                [
+                   ('num_pipeline',num_pipeline,numeric_columns) 
+                   ('cat_pipeline',cat_pipeline,categorical_columns) 
+                ]
+            )
+
+            return preprocess
+          
+
+        except Exception as e:
+            raise CustomException(e,sys)
+        
+
+
+    def initiate_data_transformation(self,train_path,test_path):
+        
+        try:
+            train_df=pd.read_csv(train_path)
+            test_df=pd.read_csv(test_path)
+
+            logging.info('Read traind and test data completed')
+
+            logging.info(' preprocess object')
+
+            preprocessing_obj=self.get_data_transformer_object()
+            
+
+            target_columnn_name='math_score'
+
+            numerical_columns=['writing_scrore','readning_scroe']
+            # Train dataset X and Y
+            input_feature_train_df=train_df.drop(columns=[target_columnn_name],axis=1)
+            target_feature_train_df=train_df[target_columnn_name]
+
+            #Test dataset
+            input_feature_test_df=test_df.drop(columns=[target_columnn_name],axis=1)
+            target_feature_test_df=test_df[target_columnn_name]
+
+
+
+            logging.info(
+                f'Applying preprocessing object on training dataframe and testing dataframe'
+            )
+            # For trainin data we applyed 'fit_transform', and for test dataset we applyed 'transform'.
+            input_feature_train_arr=preprocessing_obj.fit_transform(input_feature_train_df)
+            input_feature_test_arr=preprocessing_obj.transform(input_feature_test_df)
+
+
+
+            #This line of code is using NumPy to concatenate two arrays horizontally. 
+            #Combine feature data and target labels into a single array fro training purposes.
+            train_arr=np.c_[
+                input_feature_train_arr,
+                np.array(target_feature_train_df)
+            ]
+
+            test_arr=np.c_[
+                            input_feature_test_arr,
+                            np.array(target_feature_test_df)
+                        ]
+
+            logging.info(f'Saved preprocessing object')
+
+
+            #save pickle file
+            save_object(
+                file_path=self.data_transformation_config.preprocessor_obj_file_path,
+                obj=preprocessing_obj
+
+            )
+
+
+
+            return(
+                train_arr,
+                test_arr,
+                self.data_transformation_config.preprocessor_obj_file_path
+
+            )
+        except Exception as e:
+            raise CustomException(e,sys)
+            
