ce01c91dd02653001e11660400bb074ff4e57cc8
Erkan UCI <erkan48592@hotmail.com>
2024-05-23 15:38:49 +0100

    Pkl file converted by txt file

--- src/utils.py
+++ src/utils.py
@@ -5,6 +5,7 @@ import dill
 import numpy as np
 import pandas as pd
 
+import pickle
 
 from src.exception import CustomException
 
@@ -22,3 +23,32 @@ def save_object(file_path,obj):
         raise CustomException(e,sys)
 
 
+
+
+
+def convert_pickled_to_text(pickle_file_path):
+    """
+    Convert a pickled file to text representation.
+
+    Args:
+    - pickle_file_path (str): Path to the pickled file.
+
+    Returns:
+    - str: Text representation of the pickled content.
+    """
+    # Load the pickled object
+    with open(pickle_file_path, "rb") as f:
+        pickled_content = pickle.load(f)
+
+    # Convert the pickled content to a human-readable format (text)
+    text_representation = str(pickled_content)
+
+    return text_representation
+
+
+if  __name__=='__main__':
+    pass
+ 
+    #pickle_file_path = "artifacts/preprocessor.pkl"
+    #text_representation = convert_pickled_to_text(pickle_file_path)
+    #print(text_representation)
